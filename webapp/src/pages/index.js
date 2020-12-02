30568b586c08178aa57a76b1ad6b2cc67d8eb831
milosbayar <milosbayar114@gmail.com>
2020-12-02 21:19:51 +0700

    test index page

--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import "../utils/globals.css"
@@ -9,11 +9,16 @@ import SEO from "../components/seo"
 
 function IndexPage() {
   console.log("IsMobile: " + IsMobile());
+  const [loading, setLoading] = useState(false);
+  useEffect( () => {
+    setLoading(true);
+  });
   return (
     <Layout>
       <SEO
         title="Home"
       />
+      { loading  &&
       <BrowserView>
         <div className="mx-auto pl-10 lg:pl-20 flex justify-between min-h-screen" style={{ paddingTop: "89px", maxWidth: "1440px" }}>
           <div className="flex justify-between flex-col">
@@ -35,6 +40,8 @@ function IndexPage() {
           <img className="float-right object-right object-contain p-5" src={images.IMAGE_HOME_HERO}></img>
         </div>
       </BrowserView>
+      }
+      { loading &&
       <MobileView>
         <div className="flex flex-col justify-between min-h-screen" style={{ paddingTop: "60px" }}>
           <div className="pl-6 flex justify-between flex-col">
@@ -53,6 +60,7 @@ function IndexPage() {
           <img className="float-right w-full object-fill" src={images.IMAGE_HOME_INDEX_MOBILE}></img>
         </div>
       </MobileView>
+      }
     </Layout >
   )
 }
